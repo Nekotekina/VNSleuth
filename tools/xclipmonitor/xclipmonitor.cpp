28c42536daf1d9f45a131649faf768b4a58f0aea
Ivan Chikish <nekotekina@gmail.com>
2024-03-24 23:23:00 +0300

    xclipmonitor: usability fixes
    Use timeouts for freezy stuff.

--- tools/xclipmonitor/xclipmonitor.cpp
+++ tools/xclipmonitor/xclipmonitor.cpp
@@ -3,17 +3,26 @@
 #include <X11/extensions/Xfixes.h>
 #include <algorithm>
 #include <cerrno>
+#include <chrono>
+#include <csignal>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <fcntl.h>
+#include <poll.h>
 #include <string>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+volatile bool g_stop = false;
+
+void sigh(int) { g_stop = true; }
+
 int main()
 {
+	signal(SIGINT, sigh);
+
 	Display* disp;
 	Window root;
 	Atom clip;
@@ -29,11 +38,27 @@ int main()
 	clip = XInternAtom(disp, "CLIPBOARD", False);
 	XFixesSelectSelectionInput(disp, root, clip, XFixesSetSelectionOwnerNotifyMask);
 
-	const char* args[] = {"xsel", "-b", nullptr};
+	// -b: retrieve CLIPBOARD
+	// -t: timeout 100 ms
+	const char* args[] = {"xsel", "-b", "-t", "100", nullptr};
 
 	std::string buf, last_text;
-	while (true) {
-		XNextEvent(disp, &evt);
+	while (!g_stop) {
+		// Wait for events with 300ms timeout
+		struct pollfd pfd = {
+			.fd = ConnectionNumber(disp),
+			.events = POLLIN,
+			.revents = 0,
+		};
+
+		if (XPending(disp) > 0 || poll(&pfd, 1, 300) > 0) {
+			const auto s0 = std::chrono::steady_clock::now();
+			XNextEvent(disp, &evt);
+			const auto s1 = std::chrono::steady_clock::now();
+			if (long d = std::chrono::duration_cast<std::chrono::milliseconds>(s1 - s0).count(); d > 100) {
+				fprintf(stderr, "xclipmonitor: XNextEvent took too long: %ldms\n", d);
+			}
+		}
 
 		int opipe_fd[2]{}; // [0] Reading from xsel stdout
 		if (pipe2(opipe_fd, O_NONBLOCK) != 0) {
@@ -51,22 +76,47 @@ int main()
 
 			// Execute xsel
 			execvp(args[0], const_cast<char**>(args));
-			perror("execvp failed for 'xsel -b':");
+			perror("execvp failed for 'xsel':");
 			return 1;
 		}
 
 		// Wait for child
-		int wstatus = 0;
-		waitpid(pid, &wstatus, 0);
-		if (int res = WEXITSTATUS(wstatus)) {
-			perror("xsel failed:");
-			return res;
+		{
+			const auto s0 = std::chrono::steady_clock::now();
+			while (true) {
+				// Sleep for 5 ms, I guess there's no point in more complex waiting implementation
+				usleep(5000);
+				int wstatus = 0;
+				if (waitpid(pid, &wstatus, WNOHANG) == pid) {
+					if (int res = WEXITSTATUS(wstatus)) {
+						perror("xsel failed:");
+						return res;
+					}
+					break;
+				}
+				const auto s1 = std::chrono::steady_clock::now();
+				if (long d = std::chrono::duration_cast<std::chrono::milliseconds>(s1 - s0).count(); d > 200) {
+					// Kill xsel after 200 ms
+					kill(pid, SIGKILL);
+					waitpid(pid, nullptr, 0);
+					pid = 0;
+					break;
+				}
+			}
+
+			if (pid == 0) {
+				close(opipe_fd[0]);
+				close(opipe_fd[1]);
+				continue;
+			}
 		}
 
 		// Read with size limit
 		buf.resize(1000);
 		buf.resize(std::max<ssize_t>(0, read(opipe_fd[0], buf.data(), buf.size())));
-		if (!buf.empty() && buf.size() < 1000 && buf != last_text) {
+		if (buf.size() >= 1000)
+			buf.clear();
+		if (!buf.empty() && buf != last_text) {
 			// Filter duplications
 			last_text = buf;
 			for (auto& ch : buf) {
@@ -78,7 +128,7 @@ int main()
 				buf.erase(buf.end() - 1);
 			printf("%s\n", buf.c_str());
 			fflush(stdout);
-		} else {
+		} else if (buf.empty()) {
 			last_text.clear();
 		}
 
@@ -86,7 +136,6 @@ int main()
 		close(opipe_fd[1]);
 	}
 
-	// unreachable
 	XCloseDisplay(disp);
 	return 0;
 }
