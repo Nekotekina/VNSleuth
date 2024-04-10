8a47597a8bce768f81e23c48f283e88b10fdef9a
Ivan Chikish <nekotekina@gmail.com>
2024-04-10 19:12:10 +0300

    Update examples and README.md

--- tools/llm_qwen32.sh
+++ tools/llm_qwen32.sh
@@ -2,4 +2,4 @@
 # Fix downloaded gguf file location.
 # -c 4096 is context size. More requires more VRAM.
 # -ngl 999 is set to offload all layers to the GPU.
-llm_common.sh -m ~/Downloads/qwen1.5-14b-chat-imat-Q5_K_M.gguf -c 4096 -ngl 999 "$@"
+llm_common.sh -m ~/Downloads/qwen1.5-32b-chat-imat-IQ4_XS.gguf -c 4096 -ngl 999 "$@"
